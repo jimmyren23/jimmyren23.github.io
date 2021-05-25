ba987b76760ccc448e75b864572083469a3374e3
Jimmy Ren <40722259+jimmyren23@users.noreply.github.com>
2021-05-25 02:01:28 -0500

    Updates

---
+++ precache-manifest.40aa20a198880e0ed0c3de66b7ccadfc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b2ae7660d439b7f678ee12fc581829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1534da03d47ef1fa284e",
+    "url": "/static/css/main.98dda728.chunk.css"
+  },
+  {
+    "revision": "8b6294af5cde4cbbca24",
+    "url": "/static/js/2.3f777db6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3f777db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1534da03d47ef1fa284e",
+    "url": "/static/js/main.422eba02.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e8ed14e1cdeed375c3c22bc1c4424b6f",
+    "url": "/static/media/B612-Regular.e8ed14e1.ttf"
+  },
+  {
+    "revision": "3c25db9666d2bfd0b1ede009dd3c306d",
+    "url": "/static/media/InstaBoard.3c25db96.png"
+  },
+  {
+    "revision": "bb1ee591fdbca352d37f124d79754cff",
+    "url": "/static/media/PogaBoard.bb1ee591.png"
+  },
+  {
+    "revision": "8990d9be4f3e147e9f2332cf208670fe",
+    "url": "/static/media/RecipeBoard.8990d9be.png"
+  },
+  {
+    "revision": "2a26bb6083fb05f98ac0ce6909dca570",
+    "url": "/static/media/SpotifyBoard.2a26bb60.png"
+  },
+  {
+    "revision": "d3d22a84eadaedfccd88f2f45ae53bbe",
+    "url": "/static/media/TweetBoard.d3d22a84.png"
+  },
+  {
+    "revision": "b93d660a6e28ae22041fdd0e810f228c",
+    "url": "/static/media/emailfilled.b93d660a.svg"
+  },
+  {
+    "revision": "210cfb813d347988bcf43832b6e475ee",
+    "url": "/static/media/facebookfilled.210cfb81.svg"
+  },
+  {
+    "revision": "17156bc75b5906b92ecd399afc04010b",
+    "url": "/static/media/githubfilled.17156bc7.svg"
+  },
+  {
+    "revision": "be7b9817189f11ae9812b319a1859dbd",
+    "url": "/static/media/instagramfilled.be7b9817.svg"
+  },
+  {
+    "revision": "1ee6dbdfb229be75a2c252b24e0595c4",
+    "url": "/static/media/linkedinfilled.1ee6dbdf.svg"
+  },
+  {
+    "revision": "93b20f9096244b33835d21dc8497e559",
+    "url": "/static/media/profile.93b20f90.jpg"
+  }
+]);
